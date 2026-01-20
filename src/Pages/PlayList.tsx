9429edab3ddea2babfc09269a34165f8e7a31012
Josh Muli <mulijoshua681@gmail.com>
2026-01-20 16:48:05 +0300

    Update PlayList.tsx

--- src/Pages/PlayList.tsx
+++ src/Pages/PlayList.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export const PlaylistPage: React.FC = () => {
-  // This grabs the "vibes-chill" part from the URL
+
   const { id } = useParams(); 
 
  
@@ -13,7 +13,7 @@ export const PlaylistPage: React.FC = () => {
     <main className="main-content">
       <h2 style={{color: 'white'}}>Playlist: {title}</h2>
       <p style={{color: '#a1a1aa'}}>Songs for {id} will appear here.</p>
-      {/* You can reuse your SongCard grid here later! */}
+    
     </main>
   );
 };
